fabe764fa4feaae689c4371d812d1da8289f502c
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-15 18:03:04 +0200

    simple-project start

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,11 +1,39 @@
 <template>
-  <p>Hello !!!</p>
+    <div class="container">
+        <app-header></app-header>
+        <hr class="test">
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-export default {}
+    import Header from './Shared/Header.vue';
+    import Footer from './Shared/Footer.vue';
+    import Servers from './Server/Servers.vue';
+    import ServerDetails from './Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
 </script>
 
 <style lang="scss">
+@import 'assets/breakpoints';
+
+@media (min-width: $md) {
+ .test{
+   border-top: 1px solid red;
+ }
+}
 
 </style>
