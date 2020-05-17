8a0cfe3edc68acaa5e630f99ff64dfce153a35b1
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-17 19:23:54 +0200

    forms_tests

---
+++ vue-cli/src/Switch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div id="on" @click="swiched(true)" :class="{ active: value }">On</div>
+    <div id="off" @click="swiched(false)" :class="{ active: !value }">Off</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    swiched(isOn) {
+      this.$emit('input', isOn)
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+    #on, #off {
+        width: 40px;
+        height: 20px;
+        background-color: lightgray;
+        padding: 2px;
+        display: inline-block;
+        margin: 10px -2px;
+        box-sizing: content-box;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    #on:hover, #on.active {
+        background-color: lightgreen;
+    }
+
+    #off:hover, #off.active {
+        background-color: lightcoral;
+    }
+</style>
