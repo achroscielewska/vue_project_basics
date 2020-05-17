d76875860b2c41c69696cf10e3d00be5047281ab
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-17 12:13:03 +0200

    communication-test

--- vue-cli/src/components/UserDetail.vue
+++ vue-cli/src/components/UserDetail.vue
@@ -3,16 +3,37 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{ name }}</p>
+    <p>User Age: {{ userAge }}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetNameFN()">Reset Name</button>
   </div>
 </template>
 
 <script>
+import { eventBus } from '../main';
 export default {
   props: {
     name: {
       type: String,
       required: true
+    },
+    resetNameFN: {
+      type: Function
+    },
+    userAge: {
+      type: Number
     }
+  },
+  methods: {
+    resetName() {
+      this.name = "Agnieszka";
+      this.$emit("nameWasReset", this.name);
+    }
+  },
+  created() {
+    eventBus.$on('ageWasEdited', (data) => {
+      this.userAge = data;
+    })
   }
 };
 </script>
--- vue-cli/src/components/UserEdit.vue
+++ vue-cli/src/components/UserEdit.vue
@@ -1,15 +1,31 @@
 <template>
-    <div class="component">
-        <h3>You may edit the User here</h3>
-        <p>Edit me!</p>
-    </div>
+  <div class="component">
+    <h3>You may edit the User here</h3>
+    <p>Edit me!</p>
+    <p>User age {{ userAge }}</p>
+    <button @click="editAge">Edit age</button>
+  </div>
 </template>
 
 <script>
+import { eventBus } from "../main";
+export default {
+  props: {
+    userAge: {
+      type: Number
+    }
+  },
+  methods: {
+    editAge() {
+      this.userAge = 30;
+      eventBus.changeAge(this.userAge);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    div {
-        background-color: lightgreen;
-    }
+div {
+  background-color: lightgreen;
+}
 </style>
