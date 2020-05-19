e14845c323dc35590f96af5ca8566aab6f125a14
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-19 12:20:40 +0200

    http_test

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,39 +1,109 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr class="test">
-        <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
+  <div class="container">
+    <div class="col-xs-12 col-sm-8">
+      <h1>Http</h1>
+      <div class="form-group">
+        <label for="">Username</label>
+        <input type="text" class="form-control" v-model="user.username" />
+      </div>
+      <div class="form-group">
+        <label for="">Mail</label>
+        <input type="text" class="form-control" v-model="user.email" />
+      </div>
+      <button class="btn btn-primary" @click="sumbmit">Submit</button>
+      <hr />
+      <input type="text" class="form-control" v-model="node" />
+      <br /><br />
+      <button class="btn btn-primary" @click="getData">Get data</button>
+      <br /><br />
+      <ul class="list-group">
+        <li class="list-group-item" v-for="(user, index) in users" :key="index">
+          {{ user.username }} / {{ user.email }}
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from './Shared/Header.vue';
-    import Footer from './Shared/Footer.vue';
-    import Servers from './Server/Servers.vue';
-    import ServerDetails from './Server/ServerDetails.vue';
-
-    export default {
-        components: {
-            'app-header': Header,
-            'app-servers': Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      },
+      users: [],
+      resource: {},
+      node: "data"
+    };
+  },
+  methods: {
+    // sumbmit() {
+    //   this.$http
+    //     .post('data.json', this.user)
+    //     .then(
+    //       response => {
+    //         console.log(response);
+    //       },
+    //       error => {
+    //         console.log(error);
+    //       }
+    //     );
+    // },
+    sumbmit() {
+      this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
+    },
+    // getData() {
+    //   this.$http
+    //     .get("data.json")
+    //     .then(
+    //       response => {
+    //         return response.json();
+    //       },
+    //       error => {
+    //         console.log(error);
+    //       }
+    //     )
+    //     .then(data => {
+    //       const resultArray = [];
+    //       for (let key in data) {
+    //         resultArray.push(data[key]);
+    //       }
+    //       this.users = resultArray;
+    //     });
+    // }
+    getData() {
+      this.resource.getData({ node: this.node })
+        .then(
+          response => {
+            return response.json();
+          },
+          error => {
+            console.log(error);
+          }
+        )
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
     }
+  },
+  created() {
+    const customAcctions = {
+      saveAlt: { method: "POST", url: "alternative.json" },
+      getData: { method: "GET" }
+    };
+    this.resource = this.$resource('{node}.json', {}, customAcctions);
+  }
+};
 </script>
 
 <style lang="scss">
-@import 'assets/breakpoints';
-
-@media (min-width: $md) {
- .test{
-   border-top: 1px solid red;
- }
-}
-
+@import "assets/breakpoints";
 </style>
+
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,5 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueResource  from 'vue-resource';
+import App from './App.vue';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-test-f888b.firebaseio.com/';
+// Vue.http.interceptors.push((request, next) => {
+//   console.log(request)
+// })
 
 new Vue({
   el: '#app',
