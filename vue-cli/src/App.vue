83e267f51f3b71e0049737f6e540c6f6086bb695
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-19 11:19:50 +0200

    cleaning

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,39 +1,15 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr class="test">
-        <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
-    </div>
+  <div id="app">
+   <h1>TEST</h1>
+  </div>
 </template>
 
 <script>
-    import Header from './Shared/Header.vue';
-    import Footer from './Shared/Footer.vue';
-    import Servers from './Server/Servers.vue';
-    import ServerDetails from './Server/ServerDetails.vue';
+export default {
 
-    export default {
-        components: {
-            'app-header': Header,
-            'app-servers': Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
-    }
+}
 </script>
 
 <style lang="scss">
-@import 'assets/breakpoints';
-
-@media (min-width: $md) {
- .test{
-   border-top: 1px solid red;
- }
-}
 
 </style>
