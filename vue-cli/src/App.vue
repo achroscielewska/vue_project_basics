670f790c1a107d7a5de54081606949ffe47fc931
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-20 08:13:03 +0200

    routes - tests

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -5,7 +5,7 @@
         <h1>Routing</h1>
         <hr />
         <app-header></app-header>
-        <transition name="slide">
+        <transition name="slide" mode="out-in">
           <router-view></router-view>
         </transition>
       </div>
@@ -22,4 +22,45 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.slide-enter {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 1s;
+}
+
+.slide-leave {
+}
+
+.slide-leave-active {
+  animation: slide-out 1s ease-out;
+  transition: opacity 1s;
+  opacity: 0;
+  position: absolute;
+}
+
+.slide-move {
+  transition: transform 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
+</style>
--- vue-cli/src/routes.js
+++ vue-cli/src/routes.js
@@ -1,8 +1,15 @@
-import User from './components/user/User.vue';
+// import User from './components/user/User.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
+
+// lazily loading
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'));
+  });
+};
 
 export const routes = [
   { path: '', component: Home },
