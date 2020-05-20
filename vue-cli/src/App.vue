4453f94acd22e52068c35507c4d5b0bbfbc8a0f2
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-20 07:05:58 +0200

    router started

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div id="app">
-   <h1>TEST</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Routing</h1>
+        <hr />
+        <app-header></app-header>
+        <transition name="slide">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-export default {
+import Header from "./components/Header.vue";
 
-}
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
